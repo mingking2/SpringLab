941cf7a96b46463df38b7c2fcb5d96da565b4622
mingking2 <mingee641@gmail.com>
2025-03-09 22:07:56 +0900

    feat: 요청 동기처리 테스트코드 추가

--- src/main/java/com/springtest/sync/SyncController.java
+++ src/main/java/com/springtest/sync/SyncController.java
@@ -1,5 +1,8 @@
 package com.springtest.sync;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,28 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class SyncController {
 
+	private final ExecutorService executor = Executors.newSingleThreadExecutor(); // 단일 스레드 실행
+
+	/*
+	 * 단일 스레드로 요청을 동기적으로 받도록 테스트
+	 * */
+	@GetMapping("/sync-process")
+	public ResponseEntity<?> processSync(@RequestParam String request) {
+		executor.submit(() -> {
+			try {
+				log.info("요청 처리 시작: {} - {}", request, Thread.currentThread().getName());
+				Thread.sleep(3000); // 3초 대기
+				log.info("요청 처리 완료: {} - {}", request, Thread.currentThread().getName());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		return ResponseEntity.ok("처리 완료! 요청: " + request);
+	}
+
+	/*
+	 * 요청을 동시에 받고 스프링 내부에서 동기적으로 동작하는지 테스트
+	 * */
 	@GetMapping("/process")
 	public ResponseEntity<?> process(@RequestParam String request) throws InterruptedException {
 		log.info("요청 처리 시작: {} - {}", request, Thread.currentThread().getName());

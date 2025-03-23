c0df7c6cc5b3f977e39175c43875cda1c99ad35f
mingking2 <mingee641@gmail.com>
2025-03-23 22:53:02 +0900

    refactor: 서블릿 등록

--- src/main/java/com/springtest/SpringTestApplication.java
+++ src/main/java/com/springtest/SpringTestApplication.java
@@ -2,6 +2,10 @@ package com.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.springtest.servlet.HelloServlet;
 
 @SpringBootApplication
 public class SpringTestApplication {
@@ -10,4 +14,11 @@ public class SpringTestApplication {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
 
+	// 내장 톰캣에 서블릿 등록
+	@Bean
+	public ServletRegistrationBean<HelloServlet> helloServlet() {
+		ServletRegistrationBean<HelloServlet> servletBean =
+			new ServletRegistrationBean<>(new HelloServlet(), "/hello");
+		return servletBean;
+	}
 }
